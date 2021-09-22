13d9ef878e0987cd53647436f6a3a0952c33ec08
xiboon <sexiarz@sexiarz.pl>
2021-09-22 22:10:54 +0200

    ban and fixes

--- src/cmds/ban.ts
+++ src/cmds/ban.ts
@@ -8,34 +8,28 @@ export async function run(
 	args: string[]
 ) {
 	if (!args[0])
-		return message.reply("Członek którego chcesz wyrzucić nie istnieje!");
+		return message.reply("Członek którego chcesz zbanować nie istnieje!");
 	const member =
 		message.mentions.members?.first() ||
 		(await message.guild?.members.fetch(args[0]).catch(() => {}));
 	const reason = args.slice(1).join(" ") || "Brak powodu";
 	if (!member)
-		return message.reply("Członek którego chcesz wyrzucić nie istnieje!");
-	if (!member?.kickable)
+		return message.reply("Członek którego chcesz zbanować nie istnieje!");
+	if (!member?.bannable)
 		return message.reply(
-			"Nie można wyrzucić tego członka, sprawdź czy bot posiada permisje"
+			"Nie można zbanować tego członka, sprawdź czy bot posiada permisje"
 		);
 	if (reason.length > 1024) {
 		message.reply("Powód nie może być dłuższy niż 1024");
 	}
-	member.kick(`Wyrzucono przez ${message.author.tag} | ${reason}`);
+	member.kick(`Zbanowano przez ${message.author.tag} | ${reason}`);
 	const embed = new Embed()
-		.setTitle("Wyrzucono!")
-		.setDescription("Pomyślnie wyrzucono członka!")
+		.setTitle("Zbanowano!")
+		.setDescription("Pomyślnie zbanowano członka!")
 		.setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
-		.addField("Wyrzucono przez:", message.author.tag)
-		.addField("Wyrzucony:", member.user.tag)
+		.addField("Zbanowano przez:", message.author.tag)
+		.addField("Zbanowano:", member.user.tag)
 		.addField("Powód", reason)
 		.setFooter(client.footer);
 	message.reply({ embeds: [embed] });
 }
-export const help = {
-	name: "kick",
-	description: "Wyrzuć kogoś z serwera",
-	aliases: ["wyrzuć"],
-	category: "moderation",
-};
