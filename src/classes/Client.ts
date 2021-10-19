1ef16649cca4198d1e523f646abf4c2a2f5401f5
xiboon <sexiarz@sexiarz.pl>
2021-10-19 22:43:48 +0200

    test

--- src/classes/Client.ts
+++ src/classes/Client.ts
@@ -57,8 +57,8 @@ export default class AlcanClient extends Client {
 			stats.set("bot.users", this.users.cache.size);
 
 			setInterval(() => {
-				stats.set("bot.guilds", this.guilds.cache.size);
-				stats.set("bot.users", this.users.cache.size);
+				stats.set("bot.guilds", this.guilds.cache.size || 10);
+				stats.set("bot.users", this.users.cache.size || 10);
 				console.log(this.users.cache.size);
 			}, 10000);
 		});
