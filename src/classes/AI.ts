af24231200e2b3c7af6a045bb699832fb0247a1c
xiboon <sexiarz@sexiarz.pl>
2021-11-09 17:05:26 +0100

    sus

---
+++ src/classes/AI.ts
@@ -0,0 +1,47 @@
+import axios, { Axios, AxiosResponse } from "axios";
+import AlcanClient from "@classes/Client";
+import Embed from "@classes/Embed";
+import { TextChannel } from "discord.js";
+
+export default class AI extends Array {
+    constructor() {
+        super();
+    }
+    async add(
+        client: AlcanClient,
+        user: string,
+        msg: string,
+        text: string,
+        channel: string
+    ) {
+        this.push({ user, msg, text, channel });
+    }
+
+    async generate(client: AlcanClient, obj: any) {
+        // @ts-ignore
+        let url = await axios.get(
+            `${client.config.aiurls[0]}?token=${client.config.aitoken}`,
+            {
+                validateStatus: function (s) {
+                    return s < 503;
+                },
+            }
+        );
+        let request = await axios.get(
+            `${url.data.text}/?token=${client.config.aitoken}&topic=${obj.text}`
+        );
+        const text = request.data.text;
+        const embed = new Embed()
+            .setTitle("Wygenerowano!")
+            .setDescription("Twój tekst:\n" + text)
+            .setFooter(client.footer);
+        this.shift();
+        const channel = (await client.channels.cache.get(
+            obj.channel
+        )) as TextChannel;
+        channel.messages.cache.get(obj.msg).reply({ embeds: [embed] });
+        if (this.length > 1) {
+            this.generate(client, this[0]);
+        }
+    }
+}
---
+++ src/cmds/ai.ts
@@ -0,0 +1,30 @@
+import AlcanClient from "@classes/Client";
+import Embed from "@classes/Embed";
+import { Message } from "discord.js";
+export async function run(
+    client: AlcanClient,
+    message: Message,
+    args: string[]
+) {
+    const text = args.join(" ");
+    if (!args[0]) return message.reply("Musisz podać tekst!");
+    client.ai.add(
+        client,
+        message.author.id,
+        message.id,
+        text,
+        message.channel.id
+    );
+    let queue = client.ai.length;
+    if (queue === 0) queue = 1;
+    message.reply(
+        `Jesteś ${queue} w kolejce. Poczekaj na wygenerowanie wiadomości.`
+    );
+    client.ai.generate(client, client.ai[0]);
+}
+export const help = {
+    name: "ai",
+    description: "Wygeneruj wiadomość za pomocą sztucznej inteligencji!",
+    aliases: ["si"],
+    category: "tools",
+};
