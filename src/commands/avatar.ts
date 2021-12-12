a1c73dcada7b7776a0e63aa4786804704d0e58fc
Superchupu <miguelgurriarannieto@gmail.com>
2021-12-12 17:37:16 +0000

    avatar command

---
+++ src/commands/avatar.ts
@@ -0,0 +1,22 @@
+import FluorineClient from '@classes/Client';
+import Embed from '@classes/Embed';
+import { CommandInteraction } from 'discord.js';
+
+export async function run(
+    client: FluorineClient,
+    interaction: CommandInteraction<'cached'>
+) {
+    const member = interaction.options.getMember('user') ?? interaction.member;
+
+    const embed = new Embed()
+        .setTitle('Avatar')
+        .setFooter(client.footer)
+        .setImage(member.displayAvatarURL({ dynamic: true, size: 512 }));
+    interaction.reply({ embeds: [embed] });
+}
+export const help = {
+    name: 'avatar',
+    description: 'Pokaż avatar wybranego użytkownika',
+    aliases: ['av'],
+    category: 'tools'
+};
