3b436069fb55eea6df39740292a41ea2d4716c0b
Superchupu <miguelgurriarannieto@gmail.com>
2021-12-12 17:14:02 +0000

    8ball command

---
+++ src/commands/8ball.ts
@@ -0,0 +1,32 @@
+import FluorineClient from '@classes/Client';
+import Embed from '@classes/Embed';
+import { CommandInteraction } from 'discord.js';
+export async function run(
+    client: FluorineClient,
+    interaction: CommandInteraction
+) {
+    const question = interaction.options.getString('question');
+    const responses = [
+        'Tak',
+        'Nie',
+        'Możliwe',
+        'Prawdopodobnie',
+        'Nie jestem pewien',
+        'Wątpię'
+    ];
+
+    const embed = new Embed()
+        .setDescription(question)
+        .setFooter(client.footer)
+        .addField(
+            'Odpowiedź',
+            responses[Math.floor(Math.random() * responses.length)]
+        );
+    interaction.reply({ embeds: [embed] });
+}
+export const help = {
+    name: '8ball',
+    description: 'Magiczna kula',
+    aliases: [],
+    category: 'fun'
+};
