7edbc6eb6705b00a501742c49562009f3dc5a4d8
Superchupu <miguelgurriarannieto@gmail.com>
2021-12-24 16:44:18 +0000

    ping

--- src/cmds/ping.ts
+++ src/cmds/ping.ts
@@ -5,7 +5,7 @@ import { Message } from 'discord.js';
 export async function run(client: FluorineClient, message: Message) {
     const embed = new Embed(client)
         .setTitle('Ping')
-        .addField('Opóźnienie', `${client.ws.ping}ms`)
+        .addLocaleField({ name: 'PING', value: `${client.ws.ping}ms` })
         .setFooter(client.footer);
     message.reply({ embeds: [embed] });
 }
