57f2b8f8177efffb3ec47f5d08f425eb05fb0c78
Superchupu <miguelgurriarannieto@gmail.com>
2021-12-29 13:04:45 +0100

    stats

---
+++ src/commands/stats.ts
@@ -0,0 +1,42 @@
+import FluorineClient from '@classes/Client';
+import Embed from '@classes/Embed';
+import { CommandInteraction } from 'discord.js';
+import dayjs from 'dayjs';
+import duration from 'dayjs/plugin/duration';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import('dayjs/locale/pl');
+
+export async function run(
+    client: FluorineClient,
+    interaction: CommandInteraction
+) {
+    dayjs.extend(duration);
+    dayjs.extend(relativeTime);
+    dayjs.locale(interaction.guild.preferredLocale);
+    const uptime = dayjs.duration(client.uptime).humanize();
+    const embed = new Embed(client, interaction.guild.preferredLocale)
+        .setLocaleTitle('STATS_TITLE')
+        .addLocaleField({
+            name: 'STATS_MEMORY_USAGE',
+            value: `${(process.memoryUsage.rss() / 1_000_000).toFixed(1)} MB`
+        })
+        .addLocaleField({
+            name: 'STATS_USER_COUNT',
+            value: client.users.cache.size.toString()
+        })
+        .addLocaleField({
+            name: 'STATS_SERVER_COUNT',
+            value: client.guilds.cache.size.toString()
+        })
+        .addLocaleField({
+            name: 'STATS_UPTIME',
+            value: uptime
+        });
+    interaction.reply({ embeds: [embed] });
+}
+export const help = {
+    name: 'stats',
+    description: 'Statystyki bota',
+    aliases: ['statystyki', 'statistics'],
+    category: 'tools'
+};
