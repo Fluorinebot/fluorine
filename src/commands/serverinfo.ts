df713a876d24eed55987fea47bf54a02a3f59e34
Superchupu <miguelgurriarannieto@gmail.com>
2021-12-29 12:55:15 +0100

    serverinfo

---
+++ src/commands/serverinfo.ts
@@ -0,0 +1,40 @@
+import FluorineClient from '@classes/Client';
+import Embed from '@classes/Embed';
+import { CommandInteraction } from 'discord.js';
+
+export async function run(
+    client: FluorineClient,
+    interaction: CommandInteraction
+) {
+    const embed = new Embed(client, interaction.guild.preferredLocale)
+        .setLocaleTitle('SERVER_INFO')
+        .addLocaleField({
+            name: 'SERVER_INFO_NAME',
+            value: interaction.guild.name
+        })
+        .addLocaleField({
+            name: 'SERVER_INFO_CREATED',
+            value: `<t:${Math.round(
+                interaction.guild.createdTimestamp / 1000
+            )}>`
+        })
+        .addLocaleField({
+            name: 'SERVER_INFO_MEMBERS',
+            value: `${interaction.guild?.memberCount}`
+        })
+        .addLocaleField({
+            name: 'SERVER_INFO_CHANNELS',
+            value: `${interaction.guild?.channels.cache.size}`
+        })
+        .addLocaleField({
+            name: 'SERVER_INFO_ROLES',
+            value: `${interaction.guild?.roles.cache.size}`
+        });
+    interaction.reply({ embeds: [embed] });
+}
+export const help = {
+    name: 'serverinfo',
+    description: 'Informacje o serwerze',
+    aliases: ['server', 'si'],
+    category: 'tools'
+};
