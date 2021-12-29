196155dbcaab62478f1056b6f9b29c21aa39a105
Superchupu <miguelgurriarannieto@gmail.com>
2021-12-29 13:03:01 +0100

    skywars

--- src/commands/skywars.ts
+++ src/commands/skywars.ts
@@ -1,52 +1,47 @@
 import FluorineClient from '@classes/Client';
 import Embed from '@classes/Embed';
+import { CommandInteraction } from 'discord.js';
 import { HypixelType } from 'types/hypixel';
-import { Message } from 'discord.js';
 import axios from 'axios';
 export async function run(
     client: FluorineClient,
-    message: Message,
-    args: string[]
+    interaction: CommandInteraction
 ) {
-    if (!args[0])
-        return message.reply(
-            client.language.get(
-                message.guild.preferredLocale,
-                'HYPIXEL_NO_ARGS',
-                { command: 'bedwars' }
-            )
-        );
+    const player = interaction.options.getString('player');
+    const uuid = await axios(
+        `https://api.mojang.com/users/profiles/minecraft/${player}`
+    ).catch(() => null);
 
-    const uuid: any = await axios(
-        `https://api.mojang.com/users/profiles/minecraft/${args[0]}`
-    );
-    if (!uuid.data.id)
-        return message.reply(
-            client.language.get(
-                message.guild.preferredLocale,
+    if (!uuid)
+        return interaction.reply({
+            content: client.language.get(
+                interaction.guild.preferredLocale,
                 'HYPIXEL_INVALID_PLAYER'
-            )
-        );
+            ),
+            ephemeral: true
+        });
 
     const { data }: { data: HypixelType } = await axios(
         `https://api.hypixel.net/player?uuid=${uuid.data.id}&key=${client.config.hypixel}`
-    );
+    ).catch(() => ({ data: null }));
 
     const skyStats = data.player?.stats?.SkyWars;
     if (!skyStats) {
-        return message.reply(
-            client.language.get(
-                message.guild.preferredLocale,
+        return interaction.reply({
+            content: client.language.get(
+                interaction.guild.preferredLocale,
                 'HYPIXEL_PLAYER_NOT_FOUND'
-            )
-        );
+            ),
+            ephemeral: true
+        });
     }
-    const kd = (skyStats.kills / skyStats.deaths).toFixed(2);
-    const winratio = (skyStats.wins / skyStats.deaths).toFixed(2);
-    const embed = new Embed(client, message.guild.preferredLocale)
-        .setLocaleTitle('HYPIXEL_STATISTICS_TITLE', {
-            player: args[0]
-        })
+
+    const kd = Number((skyStats.kills / skyStats.deaths).toFixed(2));
+
+    const winratio = Number((skyStats.wins / skyStats.deaths).toFixed(2));
+
+    const embed = new Embed(client, interaction.guild.preferredLocale)
+        .setLocaleTitle('HYPIXEL_STATISTICS_TITLE', { player })
         .setDescription(`K/D: ${kd}\n Win/loss ratio: ${winratio}`)
         .addLocaleField({
             name: 'HYPIXEL_WON_GAMES',
@@ -78,7 +73,7 @@ export async function run(
         .setThumbnail(
             `https://crafatar.com/avatars/${uuid.data.id}?default=MHF_Steve&overlay`
         );
-    message.reply({ embeds: [embed] });
+    interaction.reply({ embeds: [embed] });
 }
 export const help = {
     name: 'skywars',
