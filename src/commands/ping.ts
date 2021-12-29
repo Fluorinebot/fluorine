a3e21afcf48b8fbaeebd11fbd681406a5f65814c
Superchupu <miguelgurriarannieto@gmail.com>
2021-12-29 12:52:50 +0100

    ping

---
+++ src/commands/ping.ts
@@ -0,0 +1,19 @@
+import FluorineClient from '@classes/Client';
+import Embed from '@classes/Embed';
+import { CommandInteraction } from 'discord.js';
+
+export async function run(
+    client: FluorineClient,
+    interaction: CommandInteraction
+) {
+    const embed = new Embed(client, interaction.guild.preferredLocale)
+        .setTitle('Ping')
+        .addLocaleField({ name: 'PING', value: `${client.ws.ping}ms` });
+    interaction.reply({ embeds: [embed] });
+}
+export const help = {
+    name: 'ping',
+    description: 'Opóźnienie pomiędzy discordem a botem',
+    aliases: ['opoznienie', 'pong'],
+    category: 'tools'
+};
