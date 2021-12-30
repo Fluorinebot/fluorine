3732048c9713cf40c2bf60ec398f5df4fdb4de63
Superchupu <miguelgurriarannieto@gmail.com>
2021-12-30 14:54:37 +0100

    improve the message handler

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -5,6 +5,8 @@ import r from 'rethinkdb';
 import { SettingsType } from 'types/settings';
 
 export async function run(client: FluorineClient, message: Message) {
+    if (message.author.bot) return;
+
     if (message.channel.type === 'DM') {
         return message.reply(
             client.language.get(
@@ -13,6 +15,7 @@ export async function run(client: FluorineClient, message: Message) {
             )
         );
     }
+
     // @ts-ignore
     const settings: SettingsType = await r
         .table('config')
@@ -21,29 +24,7 @@ export async function run(client: FluorineClient, message: Message) {
     const args = message.content.slice(settings.prefix.length).split(' ');
     const command = args.shift();
 
-    if (message.content.startsWith(settings.prefix)) {
-        if (client.cooldown.has(message.author.id)) {
-            const coolEmbed = new Embed(client, message.guild.preferredLocale)
-                .setLocaleTitle('MESSAGE_CREATE_COOLDOWN_TITLE')
-                .setLocaleDescription('MESSAGE_CREATE_COOLDOWN_DESCRIPTION');
-            return message.reply({ embeds: [coolEmbed] });
-        }
-        client.cooldown.add(message.author.id);
-        if (message.author.id !== '817883855310684180') {
-            setTimeout(() => {
-                client.cooldown.delete(message.author.id);
-            }, 2000);
-        }
-        const code = client.cmds.get(command);
-        if (code) {
-            code.run(client, message, args);
-            if (code.help.category !== 'dev') {
-                client.statcord.postCommand(code.help.name, message.author.id);
-            }
-        } else {
-            return message.react('❌');
-        }
-    } else if (message.content === `<@!${client.user.id}>`) {
+    if (message.content === `<@!${client.user.id}>`) {
         const embed = new Embed(client, message.guild.preferredLocale)
             .setTitle('Fluorine')
             .setLocaleDescription('MESSAGE_CREATE_DESCRIPTION', {
@@ -66,5 +47,33 @@ export async function run(client: FluorineClient, message: Message) {
                 value: client.channels.cache.size.toString()
             });
         message.channel.send({ embeds: [embed] });
+    }
+
+    if (!message.content.startsWith(settings.prefix)) {
+        return;
+    }
+
+    if (client.cooldown.has(message.author.id)) {
+        const coolEmbed = new Embed(client, message.guild.preferredLocale)
+            .setLocaleTitle('MESSAGE_CREATE_COOLDOWN_TITLE')
+            .setLocaleDescription('MESSAGE_CREATE_COOLDOWN_DESCRIPTION');
+        return message.reply({ embeds: [coolEmbed] });
+    }
+
+    client.cooldown.add(message.author.id);
+
+    if (message.author.id !== '817883855310684180') {
+        setTimeout(() => {
+            client.cooldown.delete(message.author.id);
+        }, 2000);
+    }
+
+    const code = client.cmds.get(command);
+    if (!code) return message.react('❌');
+
+    code.run(client, message, args);
+
+    if (code.help.category !== 'dev') {
+        client.statcord.postCommand(code.help.name, message.author.id);
     }
 }
