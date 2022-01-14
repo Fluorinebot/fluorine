98684169a325691ac3f1907273a0b77e29f3b5c7
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2022-01-14 14:46:23 +0000

    yeah

--- src/classes/Logger.ts
+++ src/classes/Logger.ts
@@ -1,16 +1,14 @@
 import { bold, yellow, blue, red } from 'picocolors';
+// get time in HH:mm:ss
+const [time] = new Date().toTimeString().split(' ');
 export default class Logger {
     error(msg: string) {
-        // get time in HH:mm:ss
-        const time = new Date().toTimeString().split(' ')[0];
         console.log(bold(red(`${time} ERROR`)), msg);
     }
     log(msg: string) {
-        const time = new Date().toTimeString().split(' ')[0];
         console.log(bold(blue(`${time} LOG`)), msg);
     }
     warn(msg: string) {
-        const time = new Date().toTimeString().split(' ')[0];
         console.log(bold(yellow(`${time} WARN`)), msg);
     }
 }
