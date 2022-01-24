754a98afda07176f7d540c44d5cc754b440169da
xiboon <38988733+xiboon@users.noreply.github.com>
2022-01-24 12:09:57 +0100

    yeah

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -55,6 +55,12 @@ export async function run(client: FluorineClient, message: Message) {
             modLog(client, Case, message.guild);
         }
     }
+
+    const random = Math.floor(Math.random() * 20) + 1;
+    if (random === 20)
+        message.channel.send(
+            '<:SlashCommands:934768130474004500> Use Slash Commands!\nFluorine will stop responding to prefix commands soon!'
+        );
     const args = message.content.slice(settings.prefix.length).split(' ');
     const command = args.shift();
     if (message.content.startsWith(settings.prefix)) {
@@ -102,33 +108,6 @@ export async function run(client: FluorineClient, message: Message) {
         message.channel.send({ embeds: [embed] });
     }
 
-    if (!message.content.startsWith(settings.prefix)) {
-        return;
-    }
-
-    if (client.cooldown.has(message.author.id)) {
-        const coolEmbed = new Embed(client, message.guild.preferredLocale)
-            .setLocaleTitle('MESSAGE_CREATE_COOLDOWN_TITLE')
-            .setLocaleDescription('MESSAGE_CREATE_COOLDOWN_DESCRIPTION');
-        return message.reply({ embeds: [coolEmbed] });
-    }
-
-    client.cooldown.add(message.author.id);
-
-    if (message.author.id !== '817883855310684180') {
-        setTimeout(() => {
-            client.cooldown.delete(message.author.id);
-        }, 2000);
-    }
-
     const code = client.cmds.get(command);
     if (!code) return message.react('❌');
-
-    const random = Math.floor(Math.random() * 20) + 1;
-    if (random === 20)
-        message.channel.send(
-            '<:SlashCommands:934768130474004500> Use Slash Commands!\nFluorine will stop responding to prefix commands soon!'
-        );
-
-    code.run(client, message, args);
 }
--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import FluorineClient from '@classes/Client';
 import Dashboard from '@dash/index';
 const client = new FluorineClient();
 client.init();
-new Dashboard(client).listen(3001);
+new Dashboard(client).listen(3002);
