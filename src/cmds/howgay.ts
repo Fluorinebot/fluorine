0ae8ca3b30e580e157ca7ba62f1eb98cb5f76359
sadie brooklyn <senkodev@gmail.com>
2022-01-28 08:57:49 +0300

    Update src/cmds/howgay.ts
    Co-authored-by: Superchupu <53496941+SuperchupuDev@users.noreply.github.com>

--- src/cmds/howgay.ts
+++ src/cmds/howgay.ts
@@ -12,7 +12,7 @@ export async function run(
             client.language.get(message.guild.preferredLocale, 'HOWGAY_ARGS')
         );
 
-    const thing = message.mentions.members.first() ?? args.join(' ');
+    const thing = message.mentions.users.first() ?? args.join(' ');
 
     const percent = ['<@478823932913516544>', '<@348591272476540928>'].includes(
         thing.toString()
