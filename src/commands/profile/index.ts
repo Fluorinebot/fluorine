0f4e1f1ff9614b2af3f4f184777c89d103e909ef
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2022-02-10 22:02:24 +0000

    fix profile choices not being in the right option

--- src/commands/profile/index.ts
+++ src/commands/profile/index.ts
@@ -7,20 +7,20 @@ export const data = new SlashCommandBuilder()
         command
             .setName('set')
             .setDescription('Set a profile')
-            .addStringOption(option => option.setName('field').setDescription('Field to set').setRequired(true))
             .addStringOption(option =>
                 option
-                    .setName('value')
-                    .setDescription('Value to set')
-                    .setRequired(true)
-                    .setChoices([
-                        ['Description', 'description'],
-                        ['Website', 'website'],
+                     .setName('field')
+                     .setDescription('Field to set')
+                     .setRequired(true)
+                     .setChoices([
                         ['Birthday', 'birthday'],
+                        ['Description', 'description'],
                         ['Location', 'location'],
-                        ['Pronouns', 'pronouns']
+                        ['Pronouns', 'pronouns'],
+                        ['Website', 'website']
                     ])
             )
+            .addStringOption(option => option.setName('value').setDescription('Value to set').setRequired(true))
     )
     .addSubcommand(command =>
         command
