6efd3bf0f258990f7d8c1073e727b9e4fdec45ed
xiboon <38988733+xiboon@users.noreply.github.com>
2022-02-11 09:36:31 +0100

    iguess im finished

--- src/commands/ai.ts
+++ src/commands/ai.ts
@@ -2,23 +2,47 @@ import FluorineClient from '@classes/Client';
 import { CommandInteraction } from 'discord.js';
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { Category } from 'types/applicationCommand';
+import { fetch } from 'undici';
+import Embed from '@classes/Embed';
 export async function run(client: FluorineClient, interaction: CommandInteraction) {
     const args = interaction.options.getString('start');
-    if (args.length > 65) {
+    if (args.length > 80) {
         return interaction.reply({
             content: client.i18n.t('AI_TOO_LONG', { lng: interaction.locale }),
             ephemeral: true
         });
     }
-    if (client.ai.queue.some(q => q.object.user.id === interaction.user.id)) {
-        interaction.reply({
-            content: client.i18n.t('AI_LIMIT', { lng: interaction.locale }),
-            ephemeral: true
+    if (client.aiCooldown.has(interaction.user.id)) {
+        return interaction.reply({
+            content: client.i18n.t('AI_COOLDOWN', { lng: interaction.locale })
         });
     }
     interaction.deferReply();
-    const argsbase = Buffer.from(args, 'utf8').toString('base64').replaceAll('/', '_').replaceAll('+', '-');
-    client.ai.getAI(interaction, argsbase);
+    const response = (await fetch('https://api.openai.com/v1/answers', {
+        headers: {
+            Authorization: `Bearer ${process.env.AI_TOKEN}`,
+            'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify({
+            model: 'ada',
+            question: args,
+            temperature: 0.3,
+            examples: [
+                ['Hello!', 'Hi! How are you?'],
+                ['Do you like me?', 'Of course!']
+            ],
+            // eslint-disable-next-line camelcase
+            examples_context: 'The AI likes you!',
+            documents: []
+        })
+    }).then(res => res.json())) as any;
+    const embed = new Embed(client, interaction.locale).setLocaleTitle('AI_TITLE').setDescription(response.answers[0]);
+    interaction.editReply({ embeds: [embed] });
+    client.aiCooldown.add(interaction.user.id);
+    setTimeout(() => {
+        client.aiCooldown.delete(interaction.user.id);
+    }, 10000);
 }
 export const data = new SlashCommandBuilder()
     .setName('ai')
--- src/context/ai.ts
+++ src/context/ai.ts
@@ -1,27 +1,41 @@
 import FluorineClient from '@classes/Client';
+import Embed from '@classes/Embed';
 import { ContextMenuCommandBuilder } from '@discordjs/builders';
 import { ApplicationCommandType } from 'discord-api-types';
 import { MessageContextMenuInteraction } from 'discord.js';
 export async function run(client: FluorineClient, interaction: MessageContextMenuInteraction) {
     const { content } = interaction.targetMessage;
-    if (content.length > 65) {
-        interaction.reply({
+    if (content.length > 80) {
+        return interaction.reply({
             content: client.i18n.t('AI_TOO_LONG', { lng: interaction.locale }),
             ephemeral: true
         });
     }
-    if (client.ai.queue.some(q => q.object.user.id === interaction.user.id)) {
-        interaction.reply({
-            content: client.i18n.t('AI_LIMIT', { lng: interaction.locale }),
-            ephemeral: true
+    if (client.aiCooldown.has(interaction.user.id)) {
+        return interaction.reply({
+            content: client.i18n.t('AI_COOLDOWN', { lng: interaction.locale })
         });
     }
-    interaction.deferReply();
-    const value = Buffer.from(content || 'h', 'utf8')
-        .toString('base64')
-        .replaceAll('/', '_')
-        .replaceAll('+', '-');
-    client.ai.getAI(interaction, value);
+    const response = (await fetch('https://api.openai.com/v1/answers', {
+        headers: {
+            Authorization: `Bearer ${process.env.AI_TOKEN}`,
+            'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify({
+            model: 'ada',
+            question: content,
+            temperature: 0.8
+        })
+    }).then(res => res.json())) as any;
+    const embed = new Embed(client, interaction.locale)
+        .setLocaleTitle('AI_TITLE')
+        .setLocaleDescription('AI_DESCRIPTION', { text: response.answers[0] });
+    interaction.reply({ embeds: [embed] });
+    client.aiCooldown.add(interaction.user.id);
+    setTimeout(() => {
+        client.aiCooldown.delete(interaction.user.id);
+    }, 10000);
 }
 
 export const data = new ContextMenuCommandBuilder().setName('AI').setType(ApplicationCommandType.Message);
