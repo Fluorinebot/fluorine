43a072bcd110b34fb02119b0f79f2632078ddc54
sadie brooklyn <sadiebk@fluorine.me>
2022-02-13 01:31:25 +0300

    fix the fucking

--- src/commands/avatar.ts
+++ src/commands/avatar.ts
@@ -1,68 +1,35 @@
 import FluorineClient from '@classes/Client';
 import Embed from '@classes/Embed';
-import { CommandInteraction, InteractionReplyOptions, MessageActionRow, MessageButton } from 'discord.js';
+import { CommandInteraction, GuildMember, InteractionReplyOptions, MessageActionRow, MessageButton } from 'discord.js';
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { Category } from 'types/applicationCommand';
 
 export async function run(client: FluorineClient, interaction: CommandInteraction<'cached'>) {
-    const member = interaction.options.getMember('user');
-    const user = interaction.options.getUser('user');
-    const author = interaction.member;
+    const member = interaction.options.getMember('user') ?? interaction.options.getUser('user') ?? interaction.member;
 
-    if (member) {
-        const embed = new Embed(client, interaction.locale)
-            .setLocaleTitle('AVATAR')
-            .setImage(member.displayAvatarURL({ dynamic: true, size: 512 }));
+    const embed = new Embed(client, interaction.locale)
+        .setLocaleTitle('AVATAR')
+        .setImage(member.displayAvatarURL({ dynamic: true, size: 512 }));
 
-        const replyOptions: InteractionReplyOptions = { embeds: [embed] };
+    const replyOptions: InteractionReplyOptions = { embeds: [embed] };
 
-        if (member.avatar) {
-            const row = new MessageActionRow().addComponents(
-                new MessageButton()
-                    .setCustomId(`avatar:${interaction.user.id}:${member.id}.guild`)
-                    .setLabel(client.i18n.t('AVATAR_GUILD', { lng: interaction.locale }))
-                    .setDisabled(true)
-                    .setStyle('PRIMARY'),
-                new MessageButton()
-                    .setCustomId(`avatar:${interaction.user.id}:${member.id}.user`)
-                    .setLabel(client.i18n.t('AVATAR_USER', { lng: interaction.locale }))
-                    .setStyle('PRIMARY')
-            );
+    if (member instanceof GuildMember && member.avatar) {
+        const row = new MessageActionRow().addComponents(
+            new MessageButton()
+                .setCustomId(`avatar:${interaction.user.id}:${member.id}.guild`)
+                .setLabel(client.i18n.t('AVATAR_GUILD', { lng: interaction.locale }))
+                .setDisabled(true)
+                .setStyle('PRIMARY'),
+            new MessageButton()
+                .setCustomId(`avatar:${interaction.user.id}:${member.id}.user`)
+                .setLabel(client.i18n.t('AVATAR_USER', { lng: interaction.locale }))
+                .setStyle('PRIMARY')
+        );
 
-            replyOptions.components = [row];
-        }
-
-        interaction.reply(replyOptions);
-    } else if (user) {
-        const embed = new Embed(client, interaction.locale)
-            .setLocaleTitle('AVATAR')
-            .setImage(user.displayAvatarURL({ dynamic: true, size: 512 }));
-        interaction.reply({ embeds: [embed] });
-    } else {
-        const embed = new Embed(client, interaction.locale)
-            .setLocaleTitle('AVATAR')
-            .setImage(author.displayAvatarURL({ dynamic: true, size: 512 }));
-
-        const replyOptions: InteractionReplyOptions = { embeds: [embed] };
-
-        if (author.avatar) {
-            const row = new MessageActionRow().addComponents(
-                new MessageButton()
-                    .setCustomId(`avatar:${interaction.user.id}:${author.id}.guild`)
-                    .setLabel(client.i18n.t('AVATAR_GUILD', { lng: interaction.locale }))
-                    .setDisabled(true)
-                    .setStyle('PRIMARY'),
-                new MessageButton()
-                    .setCustomId(`avatar:${interaction.user.id}:${author.id}.user`)
-                    .setLabel(client.i18n.t('AVATAR_USER', { lng: interaction.locale }))
-                    .setStyle('PRIMARY')
-            );
-
-            replyOptions.components = [row];
-        }
-
-        interaction.reply(replyOptions);
+        replyOptions.components = [row];
     }
+
+    interaction.reply(replyOptions);
 }
 
 export const data = new SlashCommandBuilder()
