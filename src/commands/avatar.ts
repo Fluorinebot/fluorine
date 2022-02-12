37b3065f456eddc050ce9595527d851dc10cf91d
sadie brooklyn <sadiebk@fluorine.me>
2022-02-12 19:18:48 +0300

    non guild users can now be shown

--- src/commands/avatar.ts
+++ src/commands/avatar.ts
@@ -5,31 +5,62 @@ import { SlashCommandBuilder } from '@discordjs/builders';
 import { Category } from 'types/applicationCommand';
 
 export async function run(client: FluorineClient, interaction: CommandInteraction<'cached'>) {
-    const member = interaction.options.getMember('user') ?? interaction.member;
+    const member = interaction.options.getMember('user');
+    const user = interaction.options.getUser('user');
+    const author = interaction.member;
 
-    const embed = new Embed(client, interaction.locale)
-        .setLocaleTitle('AVATAR')
-        .setImage(member.displayAvatarURL({ dynamic: true, size: 512 }));
+    if (member) {
+        const embed = new Embed(client, interaction.locale)
+            .setLocaleTitle('AVATAR')
+            .setImage(member.displayAvatarURL({ dynamic: true, size: 512 }));
 
-    const replyOptions: InteractionReplyOptions = { embeds: [embed] };
+        const replyOptions: InteractionReplyOptions = { embeds: [embed] };
 
-    if (member.avatar) {
-        const row = new MessageActionRow().addComponents(
-            new MessageButton()
-                .setCustomId(`avatar:${interaction.user.id}:${member.id}.guild`)
-                .setLabel(client.i18n.t('AVATAR_GUILD'))
-                .setDisabled(true)
-                .setStyle('PRIMARY'),
-            new MessageButton()
-                .setCustomId(`avatar:${interaction.user.id}:${member.id}.user`)
-                .setLabel(client.i18n.t('AVATAR_USER'))
-                .setStyle('PRIMARY')
-        );
+        if (member.avatar) {
+            const row = new MessageActionRow().addComponents(
+                new MessageButton()
+                    .setCustomId(`avatar:${interaction.user.id}:${member.id}.guild`)
+                    .setLabel(client.i18n.t('AVATAR_GUILD'))
+                    .setDisabled(true)
+                    .setStyle('PRIMARY'),
+                new MessageButton()
+                    .setCustomId(`avatar:${interaction.user.id}:${member.id}.user`)
+                    .setLabel(client.i18n.t('AVATAR_USER'))
+                    .setStyle('PRIMARY')
+            );
 
-        replyOptions.components = [row];
+            replyOptions.components = [row];
+        }
+
+        interaction.reply(replyOptions);
+    } else if (user) {
+        const embed = new Embed(client, interaction.locale)
+            .setLocaleTitle('AVATAR')
+            .setImage(user.displayAvatarURL({ dynamic: true, size: 512 }));
+        interaction.reply({ embeds: [embed] });
+    } else {
+        const embed = new Embed(client, interaction.locale)
+            .setLocaleTitle('AVATAR')
+            .setImage(member.displayAvatarURL({ dynamic: true, size: 512 }));
+
+        const replyOptions: InteractionReplyOptions = { embeds: [embed] };
+
+        if (author.avatar) {
+            const row = new MessageActionRow().addComponents(
+                new MessageButton()
+                    .setCustomId(`avatar:${interaction.user.id}:${author.id}.guild`)
+                    .setLabel(client.i18n.t('AVATAR_GUILD'))
+                    .setDisabled(true)
+                    .setStyle('PRIMARY'),
+                new MessageButton()
+                    .setCustomId(`avatar:${interaction.user.id}:${author.id}.user`)
+                    .setLabel(client.i18n.t('AVATAR_USER'))
+                    .setStyle('PRIMARY')
+            );
+
+            replyOptions.components = [row];
+        }
     }
-
-    interaction.reply(replyOptions);
 }
 
 export const data = new SlashCommandBuilder()
