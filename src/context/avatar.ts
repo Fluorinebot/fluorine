28092a50d63dfd92ac3744f13d267a0a115e407d
sadie brooklyn <sadiebk@fluorine.me>
2022-02-12 13:09:22 +0300

    add the context

--- src/context/avatar.ts
+++ src/context/avatar.ts
@@ -1,16 +1,46 @@
 import FluorineClient from '@classes/Client';
 import Embed from '@classes/Embed';
-import { UserContextMenuInteraction } from 'discord.js';
+import { UserContextMenuInteraction, InteractionReplyOptions, MessageActionRow, MessageButton } from 'discord.js';
 import { ContextMenuCommandBuilder } from '@discordjs/builders';
 import { ApplicationCommandType } from 'discord-api-types';
 
 export async function run(client: FluorineClient, interaction: UserContextMenuInteraction<'cached'>): Promise<void> {
     const user = interaction.targetMember ?? interaction.targetUser;
 
-    const embed = new Embed(client, interaction.locale)
-        .setLocaleTitle('AVATAR')
-        .setImage(user.displayAvatarURL({ dynamic: true, size: 512 }));
-    interaction.reply({ embeds: [embed] });
+    if (interaction.targetMember) {
+        const member = interaction.targetMember;
+
+        const embed = new Embed(client, interaction.locale)
+            .setLocaleTitle('AVATAR')
+            .setImage(member.displayAvatarURL({ dynamic: true, size: 512 }));
+
+        const replyOptions: InteractionReplyOptions = { embeds: [embed] };
+
+        if (member.avatar) {
+            const row = new MessageActionRow().addComponents(
+                new MessageButton()
+                    .setCustomId(`avatar:${interaction.user.id}:${member.id}.guild`)
+                    .setLabel(client.i18n.t('AVATAR_GUILD'))
+                    .setDisabled(true)
+                    .setStyle('PRIMARY'),
+                new MessageButton()
+                    .setCustomId(`avatar:${interaction.user.id}:${member.id}.user`)
+                    .setLabel(client.i18n.t('AVATAR_USER'))
+                    .setStyle('PRIMARY')
+            );
+
+            replyOptions.components = [row];
+        }
+
+        interaction.reply(replyOptions);
+    } else {
+        const user = interaction.targetUser;
+
+        const embed = new Embed(client, interaction.locale)
+            .setLocaleTitle('AVATAR')
+            .setImage(user.displayAvatarURL({ dynamic: true, size: 512 }));
+        interaction.reply({ embeds: [embed] });
+    }
 }
 
 export const data = new ContextMenuCommandBuilder().setName('Avatar').setType(ApplicationCommandType.User);
