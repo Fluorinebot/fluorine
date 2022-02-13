c7c44721337cb772a313a9f1c746cd904508586e
sadie brooklyn <sadiebk@fluorine.me>
2022-02-13 21:53:38 +0300

    auto add deploy

--- src/commands/deploy/delete.ts
+++ src/commands/deploy/delete.ts
@@ -13,7 +13,8 @@ export async function run(client: FluorineClient, interaction: CommandInteractio
         await commands.fetch();
 
         if (name === 'all') {
-            await commands.set([]);
+            if (guildId) await commands.set([(await import('../../commands/deploy/index')).data.toJSON()]);
+            else await commands.set([]);
         } else {
             const command = commands.cache.find(c => c.name === name);
             if (!command)
