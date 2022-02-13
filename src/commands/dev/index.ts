c4a020912e4ac7889f8a92bccc533761b3f9591c
sadie brooklyn <sadiebk@fluorine.me>
2022-02-13 16:37:48 +0300

    add reload

---
+++ src/commands/dev/index.ts
@@ -0,0 +1,50 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { Category } from 'types/applicationCommand';
+
+export const data = new SlashCommandBuilder()
+    .setName('dev')
+    .setDescription("Commands intended to ease in Fluorine's development.")
+    .addSubcommand(subcommand =>
+        subcommand
+            .setName('reload')
+            .setDescription('Reloads a module.')
+            .addStringOption(option =>
+                option
+                    .setName('type')
+                    .setDescription('The type of the module.')
+                    .setRequired(true)
+                    .setChoices([
+                        ['Event', 'events'],
+                        ['Chat Input Command', 'commands'],
+                        ['Context Menu Command', 'context'],
+                        ['Component', 'components']
+                    ])
+            )
+            .addStringOption(option =>
+                option.setName('module').setDescription("The module that you're reloading.").setRequired(true)
+            )
+    )
+    .addSubcommand(subcommand =>
+        subcommand
+            .setName('shell')
+            .setDescription('Execute a shell script')
+            .addStringOption(option => option.setName('script').setDescription('The shell script.').setRequired(true))
+    );
+
+if (process.env.NODE_ENV === 'development') {
+    data.addSubcommand(subcommand =>
+        subcommand
+            .setName('eval')
+            .setDescription('Evaluates a given exprssion.')
+            .addStringOption(option => option.setName('code').setDescription('The code to evaluate.').setRequired(true))
+            .addBooleanOption(option =>
+                option
+                    .setName('shell')
+                    .setDescription('Whether to execute as a shell command or Javascript code')
+                    .setRequired(false)
+            )
+    );
+}
+
+export const category: Category = 'tools';
+export const dev = true;
---
+++ src/commands/dev/reload.ts
@@ -0,0 +1,66 @@
+import FluorineClient from '@classes/Client';
+import Embed from '@classes/Embed';
+import { codeBlock } from '@discordjs/builders';
+import { CommandInteraction } from 'discord.js';
+import { execSync } from 'child_process';
+
+export async function run(client: FluorineClient, interaction: CommandInteraction) {
+    await interaction.deferReply({ ephemeral: true, fetchReply: true });
+    const type = interaction.options.getString('type');
+    const module = interaction.options.getString('module');
+
+    try {
+        execSync('npm run build');
+        delete require.cache[require.resolve(`./../../${type}/${module}.js`)];
+
+        switch (type) {
+            case 'events': {
+                const eventFile: any = await import(`./../../events/${module}`);
+
+                client.removeListener(module, eventFile.run);
+                client.on(module, (...event) => {
+                    eventFile.run(client, ...event);
+                });
+
+                interaction.editReply(`Reloaded the \`${module}\` event.`);
+                break;
+            }
+
+            case 'commands': {
+                if (module.includes('index')) {
+                    return interaction.editReply(
+                        'The reload was successful, however no re-require operations occured.'
+                    );
+                }
+
+                const commandFile = await import(`./../../commands/${module}`);
+                client.applicationCommands.chatInput.set(module, commandFile);
+
+                interaction.editReply(`Reloaded the \`${module}\` chat input command.`);
+                break;
+            }
+
+            case 'context': {
+                const commandFile = await import(`./../../context/${module}`);
+                client.applicationCommands.contextMenu.set(module, commandFile);
+
+                interaction.editReply(`Reloaded the \`${module}\` context menu command.`);
+                break;
+            }
+
+            case 'components': {
+                const commandFile = await import(`./../../components/${module}`);
+                client.components.set(module, commandFile);
+
+                interaction.editReply(`Reloaded the \`${module}\` component.`);
+                break;
+            }
+        }
+    } catch (error) {
+        const embed = new Embed(client, interaction.locale)
+            .setTitle('Epic Fail')
+            .setDescription(codeBlock('js', error.stack));
+
+        interaction.editReply({ embeds: [embed] });
+    }
+}
