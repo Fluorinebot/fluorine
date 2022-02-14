d143c482fa1fd47f3444ce11b1fdc30b1585fecc
sadie brooklyn <senkodev@gmail.com>
2022-02-14 23:03:35 +0300

    better dev tools (#110)
    * add reload
    
    * add update
    
    * add reload all
    
    * rip old update
    
    * sex
    
    * better deploy messages
    
    * import from handlers directly
    
    * auto add deploy
    
    * remove useless if
    
    * Apply suggestions from code review
    
    Co-authored-by: Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
    
    * remove useless check
    
    * dont build in prod
    
    * Apply suggestions from code review
    
    Co-authored-by: Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
    
    * neater
    
    * fix authors trolololol
    
    * add support for codeblocks
    
    slatev2 when
    
    * clean dist on build
    
    * sourcemap
    
    * enable source maps in node
    
    * revert sourcemaps
    
    * noden't
    
    * add update in dev
    
    * attempt to get index reloading working?
    
    * remove useless indent lol
    
    * fix index reloading
    
    * fix reload finally
    
    * try to fix event reload
    
    * add reload all
    
    * this
    
    * deploy2
    
    * example
    
    * space
    
    * log moment
    
    Co-authored-by: Superchupu <53496941+SuperchupuDev@users.noreply.github.com>

--- src/commands/dev/update.ts
+++ src/commands/dev/update.ts
@@ -1,18 +1,11 @@
 import FluorineClient from '@classes/Client';
 import { execSync } from 'child_process';
-import { SlashCommandBuilder } from '@discordjs/builders';
 import { CommandInteraction } from 'discord.js';
-import { Category } from 'types/applicationCommand';
 
 export async function run(client: FluorineClient, interaction: CommandInteraction) {
     await interaction.deferReply();
     execSync('git pull');
     execSync('npm run build');
     execSync('npm i');
-    interaction.editReply('done');
+    interaction.editReply('Successfully updated');
 }
-
-export const data = new SlashCommandBuilder().setName('update').setDescription('Update the bot');
-
-export const category: Category = 'tools';
-export const dev = true;
