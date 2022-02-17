97b6f1ffa5da07f77a439ea14e8a13abe2ea8907
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2022-02-17 14:51:48 +0000

    no need for this

--- src/util/files.ts
+++ src/util/files.ts
@@ -1,27 +1,8 @@
-import { dirname, join } from 'path';
-import { fileURLToPath, pathToFileURL, URL } from 'url';
+import { join } from 'path';
 import { readdir } from 'fs/promises';
 
-// most of the logic taken from https://github.com/IanMitchell/djs-template/blob/main/apps/bot/src/lib/files.ts
-
-/**
- * Call with `import.meta.url` to get the `__filename` equivalent
- */
-export function getFilename(url: URL | string) {
-    return fileURLToPath(url);
-}
-
-/**
- * Call with `import.meta.url` to get the `__dirname` equivalent
- */
-export function getDirname(url: URL | string) {
-    return dirname(getFilename(url));
-}
-
 export async function loadDirectory<T>(relativePath: string) {
-    // see https://github.com/swc-project/swc/issues/1202#issuecomment-943788993
-    const url = pathToFileURL(__filename).toString();
-    const directory = join(getDirname(url), relativePath);
+    const directory = join(__dirname, relativePath);
 
     const files = await readdir(directory);
     return Promise.all(files.map(async file => loadFile<T>(directory, file)));
