22cc06a539067ef4027144b3244ac4753762eea9
sadie mae katherine <32780515+night-lake@users.noreply.github.com>
2022-03-25 19:36:16 +0300

    Profiles
    Profile viewing works, not so much profile setting.

--- src/commands/profile/set.ts
+++ src/commands/profile/set.ts
@@ -2,140 +2,20 @@ import FluorineClient from '@classes/Client';
 import Embed from '@classes/Embed';
 import { SlashCommandSubcommandBuilder } from '@discordjs/builders';
 import { CommandInteraction } from 'discord.js';
-import r from 'rethinkdb';
+import { Profile } from 'types/databaseTables';
 
 export async function run(client: FluorineClient, interaction: CommandInteraction) {
-    const profile = await r.table('profile').get(interaction.user.id).run(client.conn);
+    const profile =
+        (
+            await client.db.query<Profile>('SELECT user_id FROM profiles WHERE user_id = $1;', [
+                BigInt(interaction.user.id)
+            ])
+        ).rows.length === 0;
+
     const field = interaction.options.getString('field');
     const value = interaction.options.getString('value');
 
     switch (field) {
-        case 'location': {
-            if (value.length > 15 || value.length < 3) {
-                return interaction.reply({
-                    content: client.i18n.t('PROFILE_INVALID_LOCATION', {
-                        lng: interaction.locale
-                    }),
-                    ephemeral: true
-                });
-            }
-
-            if (profile) {
-                await r.table('profile').get(interaction.user.id).update({ location: value }).run(client.conn);
-            } else {
-                await r
-                    .table('profile')
-                    .insert({
-                        id: interaction.user.id,
-                        location: value
-                    })
-                    .run(client.conn);
-            }
-
-            const locEmbed = new Embed(client, interaction.locale)
-                .setLocaleTitle('PROFILE_SUCCESS')
-                .setLocaleDescription('PROFILE_SET_LOCATION', {
-                    value
-                });
-            interaction.reply({ embeds: [locEmbed], ephemeral: true });
-            break;
-        }
-
-        case 'website': {
-            const website = value;
-            const regex =
-                /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,20}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/gu;
-            if (!website.match(regex)) {
-                return interaction.reply(
-                    client.i18n.t('PROFILE_INVALID_WEBSITE', {
-                        lng: interaction.locale
-                    })
-                );
-            }
-
-            if (profile) {
-                await r.table('profile').get(interaction.user.id).update({ website }).run(client.conn);
-            } else {
-                await r
-                    .table('profile')
-                    .insert({
-                        id: interaction.user.id,
-                        website
-                    })
-                    .run(client.conn);
-            }
-
-            const webEmbed = new Embed(client, interaction.locale)
-                .setLocaleTitle('PROFILE_SUCCESS')
-                .setLocaleDescription('PROFILE_SET_WEBSITE', { website });
-            interaction.reply({ embeds: [webEmbed], ephemeral: true });
-            break;
-        }
-
-        case 'pronouns': {
-            const pronouns = value;
-            if (!['she/her', 'he/him', 'they/them'].includes(pronouns)) {
-                return interaction.reply({
-                    content: client.i18n.t('PROFILE_INVALID_PRONOUNS', {
-                        lng: interaction.locale
-                    }),
-                    ephemeral: true
-                });
-            }
-
-            if (profile) {
-                await r.table('profile').get(interaction.user.id).update({ pronouns }).run(client.conn);
-            } else {
-                await r
-                    .table('profile')
-                    .insert({
-                        id: interaction.user.id,
-                        pronouns
-                    })
-                    .run(client.conn);
-            }
-
-            const pronounEmbed = new Embed(client, interaction.locale)
-                .setLocaleTitle('PROFILE_SUCCESS')
-                .setLocaleDescription('PROFILE_SET_PRONOUNS', {
-                    pronouns
-                });
-            interaction.reply({ embeds: [pronounEmbed], ephemeral: true });
-            break;
-        }
-
-        case 'description': {
-            const description = value;
-            if (description.length > 300) {
-                return interaction.reply({
-                    content: client.i18n.t('PROFILE_DESCRIPTION_LENGTH', {
-                        lng: interaction.locale
-                    }),
-                    ephemeral: true
-                });
-            }
-
-            if (profile) {
-                await r.table('profile').get(interaction.user.id).update({ description }).run(client.conn);
-            } else {
-                await r
-                    .table('profile')
-                    .insert({
-                        id: interaction.user.id,
-                        description
-                    })
-                    .run(client.conn);
-            }
-
-            const descEmbed = new Embed(client, interaction.locale)
-                .setLocaleTitle('PROFILE_SUCCESS')
-                .setLocaleDescription('PROFILE_SET_DESCRIPTION', {
-                    description
-                });
-            interaction.reply({ embeds: [descEmbed], ephemeral: true });
-            break;
-        }
-
         case 'birthday': {
             const birthday = value;
             let [day, month]: any = birthday.split('/');
@@ -154,20 +34,152 @@ export async function run(client: FluorineClient, interaction: CommandInteractio
             }
 
             if (profile) {
-                await r.table('profile').get(interaction.user.id).update({ birthday }).run(client.conn);
+                await client.db.query('UPDATE profiles SET birthday = $1 WHERE user_id = $2', [
+                    value,
+                    BigInt(interaction.user.id)
+                ]);
             } else {
-                await r
-                    .table('profile')
-                    .insert({
-                        id: interaction.user.id,
-                        birthday
-                    })
-                    .run(client.conn);
+                await client.db.query(
+                    'INSERT INTO profiles(user_id, description, pronouns, website, location, birthday) VALUES($1, null, null, null, null, $2)',
+                    [BigInt(interaction.user.id), value]
+                );
             }
 
             const embed = new Embed(client, interaction.locale)
                 .setLocaleTitle('PROFILE_SUCCESS')
                 .setLocaleDescription('PROFILE_SET_BIRTHDAY', { birthday });
+
+            interaction.reply({ embeds: [embed], ephemeral: true });
+            break;
+        }
+
+        case 'location': {
+            if (value.length > 15 || value.length < 3) {
+                return interaction.reply({
+                    content: client.i18n.t('PROFILE_INVALID_LOCATION', {
+                        lng: interaction.locale
+                    }),
+                    ephemeral: true
+                });
+            }
+
+            if (profile) {
+                await client.db.query('UPDATE profiles SET location = $1 WHERE user_id = $2', [
+                    value,
+                    BigInt(interaction.user.id)
+                ]);
+            } else {
+                await client.db.query(
+                    'INSERT INTO profiles(user_id, description, pronouns, website, location, birthday) VALUES($1, null, null, null, $2, null)',
+                    [BigInt(interaction.user.id), value]
+                );
+            }
+
+            const embed = new Embed(client, interaction.locale)
+                .setLocaleTitle('PROFILE_SUCCESS')
+                .setLocaleDescription('PROFILE_SET_LOCATION', {
+                    value
+                });
+
+            interaction.reply({ embeds: [embed], ephemeral: true });
+            break;
+        }
+
+        case 'website': {
+            const website = value;
+            const regex =
+                /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,20}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/gu;
+
+            if (!website.match(regex)) {
+                return interaction.reply(
+                    client.i18n.t('PROFILE_INVALID_WEBSITE', {
+                        lng: interaction.locale
+                    })
+                );
+            }
+
+            if (profile) {
+                await client.db.query('UPDATE profiles SET website = $1 WHERE user_id = $2', [
+                    value,
+                    BigInt(interaction.user.id)
+                ]);
+            } else {
+                await client.db.query(
+                    'INSERT INTO profiles(user_id, description, pronouns, website, location, birthday) VALUES($1, null, null, $2, null, null)',
+                    [BigInt(interaction.user.id), value]
+                );
+            }
+
+            const embed = new Embed(client, interaction.locale)
+                .setLocaleTitle('PROFILE_SUCCESS')
+                .setLocaleDescription('PROFILE_SET_WEBSITE', { website });
+
+            interaction.reply({ embeds: [embed], ephemeral: true });
+            break;
+        }
+
+        case 'pronouns': {
+            const pronouns = value;
+            if (!['she/her', 'he/him', 'they/them'].includes(pronouns)) {
+                return interaction.reply({
+                    content: client.i18n.t('PROFILE_INVALID_PRONOUNS', {
+                        lng: interaction.locale
+                    }),
+                    ephemeral: true
+                });
+            }
+
+            if (profile) {
+                await client.db.query('UPDATE profiles SET pronouns = $1 WHERE user_id = $2', [
+                    value,
+                    BigInt(interaction.user.id)
+                ]);
+            } else {
+                await client.db.query(
+                    'INSERT INTO profiles(user_id, description, pronouns, website, location, birthday) VALUES($1, null, $2, null, null, null)',
+                    [BigInt(interaction.user.id), value]
+                );
+            }
+
+            const embed = new Embed(client, interaction.locale)
+                .setLocaleTitle('PROFILE_SUCCESS')
+                .setLocaleDescription('PROFILE_SET_PRONOUNS', {
+                    pronouns
+                });
+
+            interaction.reply({ embeds: [embed], ephemeral: true });
+            break;
+        }
+
+        case 'description': {
+            const description = value;
+            if (description.length > 300) {
+                return interaction.reply({
+                    content: client.i18n.t('PROFILE_DESCRIPTION_LENGTH', {
+                        lng: interaction.locale
+                    }),
+                    ephemeral: true
+                });
+            }
+
+            if (profile) {
+                await client.db.query('UPDATE profiles SET description = $1 WHERE user_id = $2', [
+                    value,
+                    BigInt(interaction.user.id)
+                ]);
+            } else {
+                await client.db.query(
+                    'INSERT INTO profiles(user_id, description, pronouns, website, location, birthday) VALUES($1, $2, null, null, null, null)',
+                    [BigInt(interaction.user.id), value]
+                );
+            }
+
+            const embed = new Embed(client, interaction.locale)
+                .setLocaleTitle('PROFILE_SUCCESS')
+                .setLocaleDescription('PROFILE_SET_DESCRIPTION', {
+                    description
+                });
+
             interaction.reply({ embeds: [embed], ephemeral: true });
             break;
         }
