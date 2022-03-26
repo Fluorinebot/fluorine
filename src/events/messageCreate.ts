dec53e84b8403b341975cfccc93e4db60c0b8bd3
sadie mae katherine <32780515+night-lake@users.noreply.github.com>
2022-03-26 19:46:38 +0300

    readd stats

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -87,5 +87,28 @@ export async function run(client: FluorineClient, message: Message) {
                 client.cooldown.delete(message.author.id);
             }, 1000);
         }
+    } else if (message.content === `<@!${client.user.id}>` || message.content === `<@${client.user.id}>`) {
+        const embed = new Embed(client, message.guild.preferredLocale)
+            .setTitle('Fluorine')
+            .setLocaleDescription('MESSAGE_CREATE_DESCRIPTION', {
+                prefix: settings.prefix
+            })
+            .addLocaleField({
+                name: 'STATS_SERVER_COUNT',
+                value: client.guilds.cache.size.toString()
+            })
+            .addLocaleField({
+                name: 'STATS_USER_COUNT',
+                value: client.users.cache.size.toString()
+            })
+            .addLocaleField({
+                name: 'STATS_COMMAND_COUNT',
+                value: client.cmds.size.toString()
+            })
+            .addLocaleField({
+                name: 'STATS_CHANNELS_COUNT',
+                value: client.channels.cache.size.toString()
+            });
+        message.channel.send({ embeds: [embed] });
     }
 }
