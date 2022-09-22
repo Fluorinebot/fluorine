0a313aa2729c7ef926e75d5b080c176d22fb2c27
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2022-09-22 14:39:39 +0100

    remove unused strings and old db fields usage

--- src/events/guildCreate.ts
+++ src/events/guildCreate.ts
@@ -1,4 +1,3 @@
-import process from 'node:process';
 import type { FluorineClient } from '#classes';
 import type { Guild } from 'discord.js';
 
@@ -6,12 +5,9 @@ export async function run(client: FluorineClient, guild: Guild) {
     await client.prisma.config.create({
         data: {
             guildId: BigInt(guild.id),
-            prefix: process.env.DISCORD_PREFIX,
             logsEnabled: false,
             logsChannel: null,
-            logModerationActions: false,
-            antibotFactor: 0,
-            antibotAction: 'timeout'
+            logModerationActions: false
         }
     });
 }
