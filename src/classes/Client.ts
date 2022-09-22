716eef1a75a3838fe343c97f30798070e5d0e712
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2022-09-22 14:41:52 +0100

    message content intent is no longer needed

--- src/classes/Client.ts
+++ src/classes/Client.ts
@@ -33,7 +33,7 @@ export class FluorineClient extends Client {
 
     constructor() {
         super({
-            intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
+            intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
             partials: [Partials.Message],
             allowedMentions: { repliedUser: false },
             presence: {
