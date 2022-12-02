aa05ca482acb2fa3ff813a1713707a6ddff9e69f
Amelia Katherine <32780515+night-lake@users.noreply.github.com>
2022-12-02 22:12:42 +0300

    remove old deploy

--- src/_commands/deploy/index.ts
+++
@@ -1,7 +0,0 @@
-import { SlashCommandBuilder } from 'discord.js';
-import type { Category } from '#types';
-
-export const data = new SlashCommandBuilder().setName('deploy').setDescription('Deploy application commands');
-
-export const category: Category = 'tools';
-export const dev = true;
