7c92310a49514ea230949589c54772be1b54795b
Amelia Katherine <32780515+night-lake@users.noreply.github.com>
2023-01-22 19:09:19 +0300

    actually serialize the bigint

--- src/api/profile.ts
+++ src/api/profile.ts
@@ -7,13 +7,17 @@ export async function getProfile(client: FluorineClient, req: FastifyRequest, re
     const { id } = await client.oauth.verify(authorization);
 
     const profile = await client.prisma.profile.findUnique({ where: { userId: BigInt(id) } });
-    res.send(profile);
+    res.send({ userId: profile.userId.toString(), ...profile });
 }
 
 export async function patchProfile(client: FluorineClient, req: FastifyRequest, res: FastifyReply) {
     const { authorization } = req.cookies as { authorization: string };
     const { id } = await client.oauth.verify(authorization);
     const body = req.body as Partial<Profile>;
+
+    if ('userId' in body) {
+        res.status(405).send();
+    }
 
     await client.prisma.profile.update({ where: { userId: BigInt(id) }, data: body });
     res.status(204).send();
