5322efb6146b7dd7e13a82068ca47eb31b34c8ce
Amelia Katherine <32780515+night-lake@users.noreply.github.com>
2023-01-22 18:56:41 +0300

    profile route

---
+++ src/api/profile.ts
@@ -0,0 +1,20 @@
+import type { FluorineClient } from '#classes';
+import type { Profile } from '@prisma/client';
+import type { FastifyReply, FastifyRequest } from 'fastify';
+
+export async function getProfile(client: FluorineClient, req: FastifyRequest, res: FastifyReply) {
+    const { authorization } = req.cookies as { authorization: string };
+    const { id } = await client.oauth.verify(authorization);
+
+    const profile = await client.prisma.profile.findUnique({ where: { userId: BigInt(id) } });
+    res.send(profile);
+}
+
+export async function patchProfile(client: FluorineClient, req: FastifyRequest, res: FastifyReply) {
+    const { authorization } = req.cookies as { authorization: string };
+    const { id } = await client.oauth.verify(authorization);
+    const body = req.body as Partial<Profile>;
+
+    await client.prisma.profile.update({ where: { userId: BigInt(id) }, data: body });
+    res.status(204).send();
+}
