98a14f483dcbc04d653c02b34569fc037c1fad0d
Amelia Katherine <32780515+night-lake@users.noreply.github.com>
2023-02-09 20:15:53 +0300

    authorize properly LOL

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -3,12 +3,12 @@ import type { FastifyReply, FastifyRequest } from 'fastify';
 
 export async function getOauthUser(client: FluorineClient, req: FastifyRequest, res: FastifyReply) {
     const { authorization } = req.cookies as { authorization: string };
-    const { id } = await client.oauth.verify(authorization);
+    const { token } = await client.oauth.verify(authorization);
 
-    const profile = await client.oauth.getUser(id);
+    const profile = await client.oauth.getUser(token.access_token);
 
     if (!profile) {
-        res.status(404).send({ error: 'User does exist', userId: id.toString() });
+        res.status(404).send({ error: 'User does not exist' });
     }
 
     res.send(profile);
