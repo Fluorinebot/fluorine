8822d70f910c2321007d7ace45bfe1ad57d2cac3
Amelia Katherine <32780515+night-lake@users.noreply.github.com>
2023-02-10 00:22:58 +0300

    make profile actually work

--- src/api/profile/index.ts
+++ src/api/profile/index.ts
@@ -20,6 +20,9 @@ export async function getProfile(client: FluorineClient, req: FastifyRequest, re
 }
 
 export async function patchProfile(client: FluorineClient, req: FastifyRequest, res: FastifyReply) {
+    const { authorization } = req.cookies as { authorization: string };
+    const { id } = await client.oauth.verify(authorization);
+
     const body = (req.body ? JSON.parse(req.body as string) : {}) as Partial<Profile>;
     body.userId = BigInt(body.userId);
 
@@ -28,9 +31,11 @@ export async function patchProfile(client: FluorineClient, req: FastifyRequest, 
         .filter(x => x !== 'userId')
         .map(x => (updatableBody[x] = body[x]));
 
+    console.log(id);
+
     await client.prisma.profile.upsert({
-        where: { userId: body.userId },
-        create: { userId: body.userId, ...body },
+        where: { userId: BigInt(id) },
+        create: { ...body, userId: BigInt(id) },
         update: updatableBody
     });
 
